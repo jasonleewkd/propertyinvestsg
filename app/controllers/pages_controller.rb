2b764a763f29b89daee9c65966974b3e11cba754
jasonleewkd <jasonleewkd@gmail.com>
2021-05-27 01:44:46 +0800

    first iteration

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,19 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def insights
+  end
+
+  def consultation
+  end
+
+  def portfolio
+  end
+
+  def seller_marketing
+  end
+
+  def buyer_experience
+  end
+end
